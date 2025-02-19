24a7ee11ef0e8be0b45336390e9fbb7f4622d274
Carlos Daniel Zambrano <cdanzamcol@gmail.com>
2025-02-18 18:07:17 -0600

    path, process

--- 4.fs-promise.js
+++ 4.fs-promise.js
@@ -3,8 +3,6 @@
 // const readFilePromise = promisify(fs.readFile)
 const fs = require('node:fs/promises')
 
-
-
 console.log('Leyendo el primer archivo ...')
 fs.readFile('./archivo.txt', 'utf-8').then(text => {
     console.log('primer texto', text);
---
+++ 7.process.js
@@ -0,0 +1,16 @@
+//argumentos de entrada
+console.log(process.argv)
+
+//controlar proceso y salida
+//process.exit(1) 
+// con 0 todo salió bien, con 1 algo salió mal y salir
+
+// process.on('exit', () => {
+//     //limpiar recursos
+// })
+
+//current working directory
+console.log(process.cwd())
+
+//platform
+console.log(process.env.PEPITO)
